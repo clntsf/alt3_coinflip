35089e5db1b902e0b0b9266de28a27138802ea88
ctsf1 <colin.tsf@gmail.com>
2022-05-02 14:14:35 +0100

    ocd

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ def get_seq_in():               # make sure user inputs a valid combination to f
 
         print("Invalid characters in sequence! include only H and T (case-insensitive)\n")
 
+
 def get_num_flips():            # make sure user inputs a valid flips number
     while True:
         num_flips = input("Number of times to flip coins: ")
@@ -20,12 +21,14 @@ def get_num_flips():            # make sure user inputs a valid flips number
 def seq_to_int_list(seq: str):                      # format sequence str (ex. "HHHTH") to list of ints ([1,1,1,0,1])
     return [int(c == "h") for c in seq.lower()]
 
+
 def flip_for_seq(num_flips: int, seq: list[int]):   # flip coins given # times and return whether each flip matched or didn't
     seq_l = len(seq)
     seq = seq_to_int_list(seq)
 
     results = randint( 2, size=(num_flips, seq_l) )
     return [list(x)==seq for x in results]
+
 
 def cf_prob(num_flips: int = None, seq: str = None):    # get the occurrence probability of a given sequence over a given number of attempts
     if num_flips == None: num_flips = get_num_flips()
@@ -38,10 +41,12 @@ def cf_prob(num_flips: int = None, seq: str = None):    # get the occurrence pro
     print("Number of flips: {:,} \n".format(num_flips))
     print(f"Sequence occurrence frequency: {round(100*num_found/num_flips, 2)}% ({num_found}/{num_flips})\n{'*'*30}\n")
 
+
 def main():                                             # run our three tests (three heads, 3x alternating HT, user choice)
     cf_prob(num_flips=100000, seq="HHH")
     cf_prob(num_flips=1000000, seq="HTHTHT")
     cf_prob()
+
 
 if __name__ == "__main__":
     main()
